8b8ec1a55b967dbf1adae6794d0b849b64937fc1
nandukmg <nandukrishnan55555@gmail.com>
2018-10-24 15:14:11 +0530

    add proposal method

---
+++ src/data/methods/index.js
@@ -0,0 +1,31 @@
+const methods = {};
+
+methods.proposal = require('./proposal');
+// methods.people = require('./people');
+// methods.headsUnderProject = require('./heads_under_project');
+// methods.heads = require('./heads');
+// methods.loginCreden
+// tials = require('./login_credentials');
+// methods.people = require('./people');
+// methods.purchaseUnderProject = require('./purchase_under_project');
+// methods.request = require('./request');
+// methods.stocks = require('./stocks');
+
+module.exports = methods;
+
+var proposal = require('./proposal');
+
+
+//TEST
+ 
+// proposal.addProposals({
+//   project_id:3,
+//   file_no:"./",
+//   name:"manga",
+//   data:{},
+//   start_date:"2201-12-18",
+//   duration:1000
+
+// }).then(function(result){
+//   console.log(result);
+// });
---
+++ src/data/methods/proposal/index.js
@@ -0,0 +1,98 @@
+const Promise = require('bluebird');
+
+const models = require('../../models');
+// const obtainInformation = require('./obtainInformation');
+
+const proposalMethods = {};
+
+proposalMethods.addProposals = (info) => {
+  console.log('inside adding proposals');
+  return new Promise((resolve, reject) => {
+    models.proposal.create(info)
+      .then((result) => {
+        resolve(result);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
+
+// findid
+proposalMethods.findById = (project_id) => {
+  // console.log('finding by id');
+  return new Promise((resolve, reject) => {
+    models.academics.classes.findAll({
+      where:
+      { project_id },
+
+    }).then((proposals) => {
+      if (proposals) {
+        resolve(proposals);
+      } else {
+        reject(new Error('Not a valid proposal id'));
+      }
+    }).catch((err) => {
+      console.log(err);
+      reject(err);
+    });
+  });
+};
+
+proposalMethods.getAllProposals = () => new Promise((resolve,
+  reject) => {
+  models.proposal.findAll()
+    .then((result) => {
+      resolve(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      reject(err);
+    });
+});
+
+proposalMethods.updateProposal = (info, data) => new Promise((
+  resolve, reject,
+) => {
+  models.proposal.update(data, {
+    where: {
+      project_id: info.project_id,
+    },
+  })
+    .then((updated) => {
+      if (updated > 0) {
+        resolve(updated);
+      } else {
+        reject(new Error());
+        // throw ('err')
+      }
+    }).catch((error) => {
+      reject(error);
+    });
+});
+
+
+
+proposalMethods.deleteProposal = info => new Promise((
+  resolve,
+  reject,
+) => {
+  models.proposal.destroy({
+    where: {
+      project_id: info.project_id,
+
+    },
+  }).then((deleted) => {
+    if (deleted === 0) {
+      console.log('error deleted 0');
+      reject(new Error());
+    } else {
+      resolve(deleted);
+    }
+  }).catch((err) => {
+    reject(err);
+  });
+});
+
+module.exports = proposalMethods;
